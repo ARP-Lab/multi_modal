8db4c8247d103740218501457ba9cb1f27e70c12
Doohoon Kim <invi.dh.kim@gmail.com>
2023-05-01 03:33:27 +0900

    Enhanced check-up environments in MLP module

--- model/MLP.py
+++ model/MLP.py
@@ -7,19 +7,20 @@ from model.UnivarsalNN import UniversalNN
 class MLP(UniversalNN):
     def __init__(
         self,
-        zconf_path: str="",
-        zconf_id: str="",
         input_length: int=0,
         input_width: int=0,
         act_func: str="gelu",
-        last_act_off: bool=False
+        last_act_off: bool=False,
+        zconf_path: str="",
+        zconf_id: str=""
     ):
         
         super().__init__(zconf_path=zconf_path, zconf_path=zconf_path)
         
-        self.input_length = self.local_conf.input_length if self.local_conf.input_length != None else input_length
-        self.input_width = self.local_conf.input_width if self.local_conf.input_width != None else input_width
-        self.last_act_off = self.local_conf.last_act_off if self.local_conf.last_act_off != None else last_act_off
+        self.input_length = self.check_local_var("input_length", input_length)
+        self.input_width = self.check_local_var("input_width", input_width)
+        self.act_func = self.check_local_var("act_func", act_func)
+        self.last_act_off = self.check_local_var("act_func", last_act_off)
         
         _act_func = {
             "gelu" : [nn.GELU()],
@@ -30,9 +31,9 @@ class MLP(UniversalNN):
             nn.Flatten(),
             nn.Linear(self.input_length * self.input_width, self.local_conf["dim_val"]["1"]),
         ]
-        self.backbone_seq += _act_func[act_func]
+        self.backbone_seq += _act_func[self.act_func]
         
-        _dim = [(k, v) for k, v in self._conf.dim_val]
+        _dim = [(k, v) for k, v in self.local_conf["dim_val"]]
         for i in range(len(_dim) - 1):
             _last_act_off = False
             
@@ -58,10 +59,17 @@ class MLP(UniversalNN):
             fl_val: int,
             sl_val: int,
             act_func: str="gelu",
-            last_act_off: bool=False
+            last_act_off: bool=False,
+            zconf_path: str="",
+            zconf_id: str=""
         ):
             
-            super().__init__()
+            super().__init__(zconf_path=zconf_path, zconf_path=zconf_path)
+            
+            self.fl_val = self.check_local_var("fl_val", fl_val)
+            self.sl_val = self.check_local_var("sl_val", sl_val)
+            self.act_func = self.check_local_var("act_func", act_func)
+            self.last_act_off = self.check_local_var("last_act_off", last_act_off)
             
             _act_func = {
                 "gelu" : [nn.GELU()],
@@ -69,12 +77,12 @@ class MLP(UniversalNN):
             }
             
             self.backbone_seq = [
-                nn.BatchNorm1d(fl_val),
-                nn.Linear(fl_val, sl_val),
+                nn.BatchNorm1d(self.fl_val),
+                nn.Linear(self.fl_val, self.sl_val)
             ]
             
             if not last_act_off:
-                self.backbone_seq += _act_func[act_func]
+                self.backbone_seq += _act_func[self.act_func]
             
             
         def forward(self, x):
